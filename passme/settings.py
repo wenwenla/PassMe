ed1a5f280b649da95901f715742d8201590e59cc
wenwenla <wenwenla@outlook.com>
2018-04-15 08:08:29 +0800

    set mysql

--- passme/settings.py
+++ passme/settings.py
@@ -85,8 +85,12 @@ WSGI_APPLICATION = 'passme.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'passme',
+        'USER': 'root',
+        'PASSWORD': '321283',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
