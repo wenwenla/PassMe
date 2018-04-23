c89b93e59606a910a050c1fd1e53077bc97efd0e
wenwenla <wenwenla@outlook.com>
2018-04-23 11:03:42 +0800

    restore settings

--- passme/settings.py
+++ passme/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'n3@a8eb7+y_=c_)00-)%@oic6vxbl$+849z)ir3qpgx#hh5ul0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
@@ -88,7 +88,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'passme',
         'USER': 'root',
-        'PASSWORD': '123456',
+        'PASSWORD': '321283',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
